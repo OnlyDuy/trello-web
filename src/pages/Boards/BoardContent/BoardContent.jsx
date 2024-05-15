630850451091e7e40ab45df09183e72d5153b854
duyvq <vuquangduy002@gmail.com>
2024-05-15 09:25:18 +0700

    code_DnD_1
    code_DnD_1

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,18 +1,36 @@
 import Box from '@mui/material/Box'
 import ListColums from './LiistColumns/ListColums'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const [orderedColumnsState, setOrderedColumnsState] = useState([])
+
+  useEffect(() => {
+    const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+    setOrderedColumnsState(orderedColumns) 
+  }, [board])
+  
+  const handleDragEnd = (event) => {
+    console.log('handleDragEnd: ', event)
+    const { active, over } = event
+
+    if (active.id !== over.id) {
+      console.log('Kéo thả')
+    }
+  }
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColums columns={orderedColumns}/>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColums columns={orderedColumnsState}/>
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/LiistColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/LiistColumns/Column/Column.jsx
@@ -18,8 +18,20 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import ListCards from './ListCards/ListCards'
 import { mapOrder } from '~/utils/sorts'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 function Column({ column }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: column._id,
+    data: { ...column }
+  })
+
+  const dnDKitColumnStyles = {
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
   const [anchorEl, setAnchorEl] = React.useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => {
@@ -29,16 +41,22 @@ function Column({ column }) {
     setAnchorEl(null)
   }
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
+
   return (
-    <Box sx={{
-      minWidth: '300px',
-      maxWidth: '300px',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0' ),
-      ml: 2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-    }}>
+    <Box
+      ref={setNodeRef}
+      styles={dnDKitColumnStyles}
+      {...attributes}
+      {...listeners}
+      sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0' ),
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }}>
       {/* Box Column Header */}
       <Box sx={{
         height: (theme) => theme.trello.columnHeaderHeight,
